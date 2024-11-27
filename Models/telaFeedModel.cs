35f68a8d5d38a5d3d3d1f15d18e521825d452af5
Pedro Jasiel <pedrojasiel18@gmail.com>
2024-11-26 22:01:57 -0300

    att

---
+++ Models/telaFeedModel.cs
@@ -0,0 +1,13 @@
+namespace findPet.Models
+{
+    public class telaFeedModel
+    {
+        public required string Nome { get; set; }
+        public required string Raca { get; set; }
+        public required string Cor { get; set; }
+        public required string Porte { get; set; }
+        public required string LocalDesaparecimento { get; set; }
+        public required DateTime DataDesaparecimento { get; set; }
+    }
+}
+
